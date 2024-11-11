ac5a17ce62338911b4325625aa86609e18238b77
Rose Ann Domingo <106006515+futuresoft-dev-eng@users.noreply.github.com>
2024-11-11 11:07:23 +0800

    ewan

--- sidebar/sidebarRRR.php
+++ sidebar/sidebarRRR.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Symbols+Rounded" rel="stylesheet">
     <style>
         body {
-            font-family: Poppins, sans-serif;
+            font-family: Arial, sans-serif;
             background: var(--body-color);
             margin: 0;
             padding-top: 0; 
@@ -31,7 +31,6 @@
         .sidebar-logo img {
             width: 50px;
             margin: 5px;
-            margin: 0px 0px 0px -50px;
         }
         .sidebar-logo-text {
             color: white;
@@ -43,13 +42,12 @@
   
         .sidebar .nav-link {
             color: #002D44;
+            font-weight: bold;
             display: flex;
             align-items: center;
             padding: 10px 20px;
             border-radius: 0 10px 10px 0;
             transition: background-color 0.3s ease, padding 0.3s ease;
-            margin: 0px 0px 10px 0px;
-            font-size: 16px;
         }
         .sidebar .nav-link:hover {
             background-color: #4597C0;
@@ -60,108 +58,43 @@
         .sidebar .nav-link.active {
             background-color: #4597C0;
             color: #fff;
-            border-radius: 0 50px 50px 0;
-            padding: 20px 20px;
         }
         .material-symbols-rounded {
             margin-right: 10px;
             font-size: 20px;
         }
 
+   
         .user-info {
             position: absolute;
             bottom: 0;
+            left: 0;
             width: 100%;
             background-color: #d6e7f0;
             padding: 10px;
             display: flex;
+            justify-content: space-between;
             align-items: center;
             border-top: 1px solid #e9ecef;
-            gap: 10px;
         }
         .user-link {
             color: #002D44;
             text-decoration: none;
             display: flex;
             align-items: center;
-            gap: 8px;
-            flex-grow: 1;
+            margin-right: 10px;
         }
-        .user-link img {
-            width: 35px;
-            height: 35px;
-            border-radius: 50%;
+        .logout-container {
+            background-color: #02476A;
+            padding: 5px;
+            align-items: center;
         }
-        .user-name {
-            font-size: 16px;
-            color: #000000;
-        }
-        .user-role {
-            font-size: 12px;
-            color: #0C517C;
-        }
-        .arrow-icon {
-            background-color: #1a4a63;
-            color: #fff;
-            font-size: 40px;
-            padding: 20px;
+        .logout-icon {
+            color: white;
+            font-size: 30px;
             cursor: pointer;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width: 50px;
-            height: 62px;
-            margin: 668px 0px 0px 200px;
-            position: absolute;
         }
 
-        /* nadagdag */
-        .section-title {
-            font-size: 14px;
-            color: #4F8AA7;
-            padding: 5px 20px;
-            margin: 20px 0px 10px 0px;
-        }
-
-        h2 {
-            font-size: 14px;
-            color: #02476A;
-            font-weight: 500px;
-            margin: 68px 0px 0px 20px;
-        }
-
-        .header {
-            position: fixed;
-            top: 0;
-            left: 250px;
-            right: 0;
-            height: 92px;
-            background-color: #ffffff;
-            display: flex;
-            align-items: center;
-            padding: 0 20px;
-            z-index: 1000;
-        }
-        .header-title {
-            font-size: 20px;
-            font-weight: bold;
-            color: #02476A;
-        }
-        
-        .bgwaves-image {
-            height: 80%;
-            width: 2000px !important;
-        }
-
-        .header h2 {
-            margin: 65px 0px 0px 0px;
-            position: absolute;
-            width: 96.9%;
-            height: 30%;
-            background-color: #2D82AD;
-            color: #fff;
-            padding: 5px 20px;
-        }
       
         @media (max-width: 992px) and (min-width: 576px) {
             .sidebar {
@@ -234,12 +167,11 @@
         }
     </style>
 </head>
-
 <body>
     <div class="top-bar d-md-none">
         <div class="top-logo">
             <img src="images/Floodpinglogo.png" alt="FloodPing">
-            <img src="images/Floodpinglogo.png" alt="Bagbag">
+            <img src="images/bagbaglogo.png" alt="Bagbag">
         </div>
         <div class="d-flex align-items-center">
             <a href="#" class="nav-link">
@@ -287,15 +219,11 @@
                         <span class="material-symbols-rounded">article</span> <span>Reports</span>
                     </a>
                 </li>
-
-                <div class="section-title">Flood Monitoring</div>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <span class="material-symbols-rounded">videocam</span> <span>Live Camera</span>
                     </a>
                 </li>
-
-                <div class="section-title">Alerts Management</div>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <span class="material-symbols-rounded">notifications_active</span> <span>Flood Alerts</span>
@@ -313,27 +241,22 @@
                 </li>
             </ul>
         </div>
-
-        <h2>Logged in as:</h2>
         <div class="user-info">
             <a href="#" class="user-link">
-                <span class="material-symbols-rounded user-icon">account_circle</span>
-                <div>
-                    <div class="user-name">JUNGWON YANG</div>
-                    <div class="user-role">Local Authority</div>
-                </div>
+                <span class="material-symbols-rounded">account_circle</span> 
+                <span class="user-name">JUNGWON YANG</span><br>
+                <span class="user-role">Local Authority</span>
             </a>
+            <div class="logout-container">
+                <span class="material-symbols-rounded logout-icon">logout</span>
+            </div>
         </div>
-        <span class="material-symbols-rounded arrow-icon">chevron_right</span>
     </nav>
 
-    <header class="header">
-        
-        <img class="bgwaves-image" src="images/bgwaves.jpg" alt="Description of the image">  
-        <h2>FLOODPING: FLOOD MONITORING AND ALERT SYSTEM</h2>
-    </header>
-
-    
+    <div class="content">
+        <h1>Welcome to Floodping</h1>
+        <p>hotdog</p>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
