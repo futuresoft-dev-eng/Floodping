80be82fea83b55543cf9837ceb47a285b7593231
Rose Ann Domingo <106006515+futuresoft-dev-eng@users.noreply.github.com>
2024-11-25 21:17:16 +0800

    Formatted privacy-policy.php: removed extra spaces and blank lines

--- HOME/privacy-policy.php
+++ HOME/privacy-policy.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Floodping | Privacy and Policy</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
     <link rel="icon" href="/floodping/images/Floodpinglogo.png" type="image/png">
 </head>
-
 <style>
     * {
     margin: 0;
@@ -359,8 +358,4 @@ body {
     </div>
 </footer>
 </body>
-
 </html>
-
-
-
